3c1db99859e9373eba3a76a1fea9f20324579211
Felix Mansel <ke52usel@studserv.uni-leipzig.de>
2023-10-19 14:45:06 +0200

    Add basic backend setup

---
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/CreditPortApplication.java
@@ -0,0 +1,13 @@
+package de.swtp13.creditportbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditPortApplication.class, args);
+	}
+
+}
---
+++ creditport-backend/src/test/java/de/swtp13/creditportbackend/CreditPortApplicationTests.java
@@ -0,0 +1,13 @@
+package de.swtp13.creditportbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditPortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
