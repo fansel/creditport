d9e8b816d92b006281097bf82a54f87c2237d5e1
Elias <elias.taibi.haschke@gmail.com>
2023-10-21 16:05:08 +0200

    Impressum und FAQ Seite hinzugefügt

---
+++ frontend/src/routes/(public)/faq/+page.svelte
@@ -0,0 +1,73 @@
+<script></script>
+
+<div class="px-4 pt-5 mt-5 mb-2 text-center">
+  <h1 class="display-4 fw-bold">FAQ's</h1>
+</div>
+<div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+        Was ist der allgemeine Ablauf des Antragsverfahrens?
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Die Anerkennung von Studienleistungen von einer anderen Universität oder Fachhochschule an der Universität Leipzig erfolgt nur für Module, die an unserer Universität angeboten werden. Um diesen Prozess in Gang zu setzen, benötigen wir von Ihnen sämtliche relevante Unterlagen und Nachweise über die bereits absolvierten Studienleistungen, wie beispielsweise ein Transcript of Records und Modulbeschreibungen. Diese Dokumente müssen im PDF-Format vorliegen und werden im späteren Antragsverfahren eingereicht. Eine Kommission wird dann über die Anerkennung Ihrer Studienleistungen für eins oder mehrere Module entscheiden.
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Warum weichen die Anzahl der Leistungspunkte (LP) für ein Modul an meiner Universität von denen an der Universität Leipzig ab?
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Die Unterschiede in der Anzahl der Leistungspunkte (LP) zwischen Ihrer Universität und der Universität Leipzig können verschiedene Gründe haben. An unserer Universität Leipzig sind Module in der Regel auf 5 oder 10 Leistungspunkte standardisiert. Es kann jedoch sein, dass ein Modul an Ihrer aktuellen Universität, das viele Inhalte umfasst, an unserer Universität in mehrere kleinere Module aufgeteilt wurde, die jeweils 5 oder 10 LP vergeben. Gleichzeitig kann es auch umgekehrt der Fall sein, dass ein Modul an Ihrer Universität, das bei uns als mehrere separate Module zählt, in Summe mehr Leistungspunkte aufweist. Unsere Sachbearbeiter und der Ausschuss werden diese Unterschiede bei der Anrechnung berücksichtigen und entsprechend entscheiden.
+      </div>
+    </div>
+  </div>
+  
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Was ist die Deadline?
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Die Fristen für eine Bewerbung variieren je nach Studiengang und der Zulassungesbeschränkung. Einige Studiengänge haben selbst in höheren Semestern noch eine Zulassungsbeschränkung. Welche Studiengänge dies betrifft erfahren sie auf den Seiten unserer Studiengänge. Dementsprechend ist es ratsam frühzeitig Ihren Antrag zu stellen, sodass alles problemlos funktionieren kann.
+      </div>
+    </div>
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+        Warum ist das alles so kompliziert? :(
+      </button>
+    </h2>
+    <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Das Anrechnen von Studienleistungen beim Wechsel zu einer neuen Universität kann kompliziert sein. Dies liegt an Unterschieden in Lehrplänen, Bewertungssystemen und individuellen Studienverläufen. Die Verwaltung des Prozesses erfordert Zeit und Ressourcen von Universitäten, und regulatorische Aspekte können den Wechsel zusätzlich erschweren. Trotzdem ist es möglich, Studienleistungen erfolgreich anrechnen zu lassen, wenn der Wechsel gut geplant wird. Dafür dient unter anderem diese Seite, die es ermöglicht den Antragsprozess zu starten. Hierfür ist es wichtig, dass sie den Anweisungen für die einzelnen Anträge genau befolgen und bereits im voraus überlegen für welche Module an der Universität Leipzig Sie Ihre Studienleistungen anrechnen lassen können.
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Accordion Point
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingNUMBER">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNUMBER" aria-expanded="false" aria-controls="collapseNUMBER">
+       Hier kommt die Frage hin
+      </button>
+    </h2>
+    <div id="collapseNUMBER" class="accordion-collapse collapse" aria-labelledby="headingNUMBER" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        lorem ipsum
+      </div>
+    </div>
+  </div>
+-->
