41237a3eb0d91123fad918b216913c5def10c8c4
Leopold F <silkeackermann35@web.de>
2023-11-13 13:18:24 +0100

    changed api_endpoint to env and renamed postgres to db

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -1,3 +1,6 @@
+import { env } from '$env/dynamic/public'
+
+
 export const title = 'Anrechungsportal';
 
-export const api_endpoint = 'http://localhost:8080/api';
+export const api_endpoint = env.PUBLIC_API_ENDPOINT || 'http://backend:8080/api';
