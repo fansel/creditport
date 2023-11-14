31a5ea80eddb8b84f00dff6367be6203b7cdf624
Daniel Bode <daniel@bode-leipzig.de>
2023-11-14 12:28:01 +0100

    change type of UniversityRepository

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/universities/UniversityRepository.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/universities/UniversityRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UniversityRepository extends JpaRepository<University,String> {
+public interface UniversityRepository extends JpaRepository<University, Integer> {
 }
