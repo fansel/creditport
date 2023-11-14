11ec7ae002c838fb4d4480e92fe7c301f05c20f8
Daniel Bode <daniel@bode-leipzig.de>
2023-11-14 12:55:23 +0100

    finish implementation of JwtAuthenticationFilter

---
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package de.swtp13.creditportbackend.config;
+
+import de.swtp13.creditportbackend.users.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
