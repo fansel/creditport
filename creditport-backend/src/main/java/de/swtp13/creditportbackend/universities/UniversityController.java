8fac459b42c980ef39f5e0b996a3b4f5ff520d23
Felix Mansel <56174409+fansel@users.noreply.github.com>
2023-11-16 18:01:28 +0100

    Add Uni controller

---
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/universities/UniversityController.java
@@ -0,0 +1,28 @@
+package de.swtp13.creditportbackend.universities;
+import de.swtp13.creditportbackend.universities.University;
+import de.swtp13.creditportbackend.universities.UniversityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/universities")
+public class UniversityController {
+
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    @GetMapping
+    public List<University> getAllUniversities(@RequestParam(required = false) String name) {
+/*        if (name != null && !name.isEmpty()) {
+            System.out.println("Get Universities with uniName like: " + name);
+            return universityRepository.findByUniNameContaining(name);
+        } else {
+            System.out.println("Get all Universities");
+            return universityRepository.findAll();
+        }*/
+    }
+}
--- creditport-backend/src/main/java/de/swtp13/creditportbackend/universities/UniversityRepository.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/universities/UniversityRepository.java
@@ -1,8 +1,9 @@
 package de.swtp13.creditportbackend.universities;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UniversityRepository extends JpaRepository<University, Integer> {
+    List<University> findByUniNameContaining(String uniName);
 }
