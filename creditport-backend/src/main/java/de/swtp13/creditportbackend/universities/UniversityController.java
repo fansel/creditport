6860b0bdae9078b1cd104025c51d2a65ff5a904c
Felix Mansel <56174409+fansel@users.noreply.github.com>
2023-11-16 19:06:16 +0100

    Fix Issue with Controller

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/universities/UniversityController.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/universities/UniversityController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,5 @@ public class UniversityController {
             return universityRepository.findAll();
         }
     }
+
 }
