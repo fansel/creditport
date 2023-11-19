08f6f62c87d11828d9675a0ee37d54d7fc52671b
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-19 12:10:53 +0100

    test to eliminate error

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
@@ -4,13 +4,12 @@ package de.swtp13.creditportbackend.procedures;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-
 import java.util.List;
 
 public interface ProcedureRepository extends JpaRepository<Procedure, String> {
-    @Query("SELECT p.procedureId, p.status, p.courseName FROM Procedure p" +
-            "JOIN FETCH p.university u" +
-            "JOIN FETCH p.request r " +
-            "JOIN FETCH r.module m ")
+    //@Query("SELECT p.procedureId, p.status, p.courseName FROM Procedure p" +
+    //        "JOIN FETCH p.university u" +
+    //        "JOIN FETCH p.request r" +
+    //        "JOIN FETCH r.module m")
     List<Procedure> findAllWithDetails();
 }
