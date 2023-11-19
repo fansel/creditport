d778a7c475969a16a18686c97f63b4ff9d5ae19d
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-19 11:48:10 +0100

    add missing import

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
@@ -2,6 +2,10 @@ package de.swtp13.creditportbackend.procedures;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+
+import java.util.List;
 
 public interface ProcedureRepository extends JpaRepository<Procedure, String> {
     @Query("SELECT p.procedureId, p.status, p.courseName FROM Procedure p" +
