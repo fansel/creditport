83dbd9d0ed4c9fede8cccfc4fcecf4faeb5f67bc
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-19 11:06:20 +0100

    update query

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
@@ -4,5 +4,9 @@ package de.swtp13.creditportbackend.procedures;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProcedureRepository extends JpaRepository<Procedure, String> {
-
+    @Query("SELECT p.procedureId, p.status, p.courseName FROM Procedure p" +
+            "JOIN FETCH p.university u" +
+            "JOIN FETCH p.request r " +
+            "JOIN FETCH r.module m ")
+    List<Procedure> findAllWithDetails();
 }
