10ec28f48f0c395f5c12abdb535cc0595e8b12ee
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-19 13:50:17 +0100

    add JavaDoc

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/requests/RequestRepository.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/requests/RequestRepository.java
@@ -3,6 +3,12 @@ package de.swtp13.creditportbackend.requests;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Dieses Interface ist ein Repository für Module.
+ * Es stellt Methoden zum Zugriff auf die Datenbank zur Verfügung.
+ * Es erbt von JpaRepository, welches bereits Methoden zum Zugriff auf die Datenbank bereitstellt.
+ * Die Methoden sind in der Dokumentation von JpaRepository zu finden.
+ */
 @Repository
 public interface RequestRepository extends JpaRepository<Request,String> {
 }
