c6334e2503e4f05ebcf9c9b781ddfcb95dcc0d98
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-19 13:49:06 +0100

    add JavaDoc

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureService.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureService.java
@@ -4,12 +4,19 @@ import de.swtp13.creditportbackend.procedures.util.IDGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * Die Klasse enthält alle Methoden zu Vorgängen.
+ */
 @Service
 public class ProcedureService {
 
     @Autowired
     private ProcedureRepository procedureRepository;
 
+    /**
+     * Die Methode generiert einen neuen Vorgang und setzt die ID.
+     * @return gibt den neuen Vorgang zurück.
+     */
     public Procedure createProcedure() {
         String id;
         do {
