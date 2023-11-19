541f3046981e81d953cb6792189f35ded1e2bd38
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-19 12:39:53 +0100

    query update 3

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/ProcedureRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProcedureRepository extends JpaRepository<Procedure, String> {
-    @Query("SELECT p FROM procedures p")
+    @Query("SELECT p FROM Procedure p")
     List<Procedure> findAllWithDetails();
 }
