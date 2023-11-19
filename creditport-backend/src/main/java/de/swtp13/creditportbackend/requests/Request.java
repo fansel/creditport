5d68daad3ec5c1343dc2e4ab3a8bf22d52bb07fe
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-19 13:50:07 +0100

    add JavaDoc

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/requests/Request.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/requests/Request.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Die Klasse repräsentiert einen Antrag.
+ */
 @Entity(name = "Requests")
 @Table(name = "requests")
 @Data
