adbac9cc95df5d8ced5ff926f0f00239d6b22ffa
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-21 12:03:55 +0100

    status changed to int

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/Status.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/Status.java
@@ -7,25 +7,28 @@ package de.swtp13.creditportbackend.procedures;
  */
 public enum Status {
     /**
+     * NEU (Studienbüro)
      * Der Status neu wird für Vorgänge verwendet, welche das Studienbüro noch nicht gesehen hat.
      */
-    NEU,
+    1,
     /**
+     * OFFEN (Studienbüro)
      * Wird für Vorgänge verwendet, welche das Studienbüro schon mal gesehen hat, aber noch nicht angefangen hat zu bearbeiten.
      */
-    OFFEN,
+    2,
     /**
+     * IN BEARBEITUNG (Studienbüro, Antragsteller_in)
      * Vorgänge, bei denen schon einzelne Anträge bearbeitet wurden
-     *
-     * Dieser Status ist vom Antragsteller sichtbar.
      */
-    IN_BEARBEITUNG,
+    3,
     /**
+     * WEITERGELEITET (Studienbüro), VORBEREITET (Prüfungsausschuss)
      * Alle Anträge in diesem Vorgang wurden vom Studienbüro bearbeitet und wurde an den Prüfungsausschuss weitergeleitet zur Bearbeitung.
      */
-    WEITERGELEITET,
+    4,
     /**
-     * alle Anträge in dem Vorgang wurden vom Prüfungsausschuss bearbeitet und es wurde eine begründete Entscheidung getroffen.
+     * VOLLSTÄNDIG (Prüfungsausschuss)
+     * Alle Anträge in dem Vorgang wurden vom Prüfungsausschuss bearbeitet und es wurde eine begründete Entscheidung getroffen.
      */
-    VOLLSTÄNDIG;
+    5;
 }
