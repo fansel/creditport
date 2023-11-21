3f056fe355e7ad1b96713d29c2da1024f9f06b40
Felix Mansel <56174409+fansel@users.noreply.github.com>
2023-11-21 16:37:12 +0100

    Debbuging (will fail)

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/CreditPortApplication.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/CreditPortApplication.java
@@ -2,8 +2,10 @@ package de.swtp13.creditportbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class CreditPortApplication {
 
 	public static void main(String[] args) {
--- creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/config/ApiConfig.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/config/ApiConfig.java
@@ -1,9 +1,15 @@
 package de.swtp13.creditportbackend.v1.config;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
 @RequestMapping("/api/v1/") // Basispfad für alle Ressourcen unter "v1"
 public class ApiConfig {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Basis-URL /api/v1/ wurde gefunden.");
+    }
 }
