5ab5d0ab30a6af0756c05889597ec964511a2427
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-21 12:14:02 +0100

    status changed to int error

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/requests/Status.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/requests/Status.java
@@ -9,26 +9,36 @@ public enum Status {
      * NICHT_BEARBEITET (Studienbüro)
      * Der Antrag wurde vom Studienbüro noch nicht bearbeitet.
      */
-    6,
+    NICHT_BEARBEITET(6),
     /**
      * BEARBEITET (Studienbüro)
      * Der Antrag wurde bearbeitet, kann an den Prüfungsausschuss weitergeleitet werden
      */
-    7,
+    BEARBEITET(7),
     /**
      * RÜCKFRAGE_NÖTIG (Studienbüro, Antragsteller_in)
      * Das ist ein Sonderstatus, wenn zu den Anträgen noch Informationen fehlen, kann das Studienbüro den Antrag manuell auf
      * "Rückfrage nötig" stellen, was dann dem Antragsteller angezeigt wird.
      */
-    8,
+    RÜCKFRAGE_NÖTIG(8),
     /**
      * ANGENOMMEN (Prüfungsausschuss, Antragsteller_in)
      * Der Prüfungsausschuss hat den Antrag als korrekt bestätigt.
      */
-    9,
+    ANGENOMMEN(9),
     /**
      * ABGELEHNT (Prüfungsausschuss, Antragsteller_in)
      * Der Prüfungsausschuss lehnt den Antrag ab.
      */
-    10;
+    ABGELEHNT(10);
+
+    private final int value;
+
+    Status(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
