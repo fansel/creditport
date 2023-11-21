2738a3a54cf4dd1efea2bc4ec4cd29a3f81bd288
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-21 12:14:17 +0100

    status changed to int error

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/Status.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/procedures/Status.java
@@ -10,25 +10,35 @@ public enum Status {
      * NEU (Studienbüro)
      * Der Status neu wird für Vorgänge verwendet, welche das Studienbüro noch nicht gesehen hat.
      */
-    1,
+    NEU(1),
     /**
      * OFFEN (Studienbüro)
      * Wird für Vorgänge verwendet, welche das Studienbüro schon mal gesehen hat, aber noch nicht angefangen hat zu bearbeiten.
      */
-    2,
+    OFFEN(2),
     /**
      * IN BEARBEITUNG (Studienbüro, Antragsteller_in)
      * Vorgänge, bei denen schon einzelne Anträge bearbeitet wurden
      */
-    3,
+    IN_BEARBEITUNG(3),
     /**
      * WEITERGELEITET (Studienbüro), VORBEREITET (Prüfungsausschuss)
      * Alle Anträge in diesem Vorgang wurden vom Studienbüro bearbeitet und wurde an den Prüfungsausschuss weitergeleitet zur Bearbeitung.
      */
-    4,
+    WEITERGELEITET(4),
     /**
      * VOLLSTÄNDIG (Prüfungsausschuss)
      * Alle Anträge in dem Vorgang wurden vom Prüfungsausschuss bearbeitet und es wurde eine begründete Entscheidung getroffen.
      */
-    5;
+    VOLLSTÄNDIG(5);
+
+    private final int value;
+
+    Status(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
 }
