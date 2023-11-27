c9529ac1ba67d517b37d6ae47031061563ef9def
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-11-27 23:19:01 +0100

    new APIs POST, GET(Id), PUT(Id)

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/procedures/ProcedureController.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/procedures/ProcedureController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -19,9 +24,33 @@ public class ProcedureController {
 
     @Autowired
     private ProcedureRepository procedureRepository;
-
+    private final ProcedureService procedureService;
 
     /**
+     * Constructs a {@code ProcedureController} with the necessary service.
+     *
+     * @param procedureService the service that will handle the business logic for {@code Procedure} entities.
+     */
+    @Autowired
+    public ProcedureController(ProcedureService procedureService) {
+        this.procedureService = procedureService;
+    }
+
+    /**
+     * POST
+     * Creates a new {@code Procedure} entity based on the provided data.
+     *
+     * @param procedure the {@code Procedure} object containing the data for the new entity.
+     * @return a {@code ResponseEntity} containing the created {@code Procedure} and the HTTP status.
+     */
+    @PostMapping
+    public ResponseEntity<Procedure> createProcedure(@RequestBody Procedure procedure) {
+        Procedure newProcedure = procedureService.createProcedure(procedure);
+        return new ResponseEntity<>(newProcedure, HttpStatus.CREATED);
+    }
+
+    /**
+     * GET
      * @author Maike
      * @return List of procedures in JSON Format
      */
@@ -29,5 +58,29 @@ public class ProcedureController {
     public ResponseEntity<List<Procedure>> getAllProceduresWithDetails() {
         List<Procedure> procedures = procedureRepository.findAllWithDetails();
         return ResponseEntity.ok(procedures);
+    }
+
+    /**
+     * GET by ID
+     * @author Maike
+     * @return List of procedures in JSON Format
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Procedure> getProcedureById(@PathVariable String procedureId) {
+        return procedureRepository.findById(procedureId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    /**
+     * PUT by ID
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<Procedure> updateProcedure(@PathVariable String procedureId, @RequestBody Procedure procedure) {
+        if (!procedureService.existsById(procedureId)) {
+            return ResponseEntity.notFound().build();
+        }
+        Item updatedProcedure = procedureService.updateProcedure(procedureId, procedure);
+        return ResponseEntity.ok(updatedProcedure);
     }
 }
