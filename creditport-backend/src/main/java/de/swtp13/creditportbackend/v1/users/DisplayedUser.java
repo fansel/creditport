f76f65708a8dfc048c8aae20c3c7bc32eb54cd99
Felix Mansel <56174409+fansel@users.noreply.github.com>
2023-11-27 13:38:55 +0100

    Merged Dev-struc in dev

---
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/users/DisplayedUser.java
@@ -0,0 +1,29 @@
+package de.swtp13.creditportbackend.v1.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DisplayedUser {
+    private int userId;
+    private String username;
+    private String role;
+
+    public static DisplayedUser of(User user) {
+        return new DisplayedUser(user.getUserId(), user.getUsername(), user.getRole().name());
+    }
+
+    public static Optional<DisplayedUser> of(Optional<User> user) {
+        if (user.isEmpty()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(DisplayedUser.of(user.orElseThrow()));
+        }
+    }
+
+}
