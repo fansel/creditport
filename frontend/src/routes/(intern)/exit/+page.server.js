70ef79c89935643b91b151ab65a8af4dbb51bfb6
Jensberger90 <silkeackermann35@web.de>
2023-11-30 12:46:13 +0100

    try to fix logout error

--- frontend/src/routes/(intern)/exit/+page.server.js
+++ frontend/src/routes/(intern)/exit/+page.server.js
@@ -1,9 +1,9 @@
-import * as api from '$lib/api.js';
+import * as config from '$lib/config.js';
 import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ cookies, locals }) {
-  cookies.delete('jwt', { path: '/' });
+  cookies.delete('jwt', { secure: config.secure_connection, path: '/' });
   locals.user = null;
 
   throw redirect(300, '/');
