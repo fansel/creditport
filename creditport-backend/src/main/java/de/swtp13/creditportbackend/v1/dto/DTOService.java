058314f2d375976643911bf434867b61a38a59b4
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-12-04 16:03:49 +0100

    DTO Service

---
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/dto/DTOService.java
@@ -0,0 +1,42 @@
+package de.swtp13.creditportbackend.v1.dto;
+
+import de.swtp13.creditportbackend.v1.dto.ProcedureRequestDTO;
+import de.swtp13.creditportbackend.v1.dto.DTORepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+import java.sql.Timestamp;
+
+@Service
+public class DTOService {
+
+    @Autowired
+    private DTORepository procedureRequestRepository;
+
+    public DTOService(DTORepository procedureRequestRepository) {
+        this.procedureRequestRepository = procedureRequestRepository;
+    }
+
+    public List<ProcedureRequestDTO> getProcedureRequests() {
+        List<Object[]> results = procedureRequestRepository.findAllProcedureRequestDTOs();
+        List<ProcedureRequestDTO> dtos = new ArrayList<>();
+        for (Object[] result : results) {
+            dtos.add(new ProcedureRequestDTO(
+                    (String) result[0], // id
+                    (String) result[1], // status
+                    (String) result[2], // proc_annotation
+                    ((Timestamp) result[3]).toLocalDateTime(), // proc_created_on
+                    (Integer) result[4], // request_id
+                    (String) result[5], // external_module_id
+                    (String) result[6], // internal_module_id
+                    (String) result[7], // req_annotation
+                    (Integer) result[8], // credit_points
+                    ((Timestamp) result[9]).toLocalDateTime() // req_created_on
+            ));
+        }
+        return dtos;
+    }
+}
