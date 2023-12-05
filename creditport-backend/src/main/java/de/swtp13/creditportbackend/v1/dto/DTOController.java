bb9f7d89f85cbfb4d086efe29bbb5d3de384cc79
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-12-05 12:58:30 +0100

    clean up code

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/dto/DTOController.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/dto/DTOController.java
@@ -1,18 +1,10 @@
 package de.swtp13.creditportbackend.v1.dto;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.http.HttpStatus;
-
 
 import java.util.List;
 
@@ -24,16 +16,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/dto")
+@RequiredArgsConstructor
 public class DTOController {
 
-    @Autowired
-    private DTORepository dtoRepository;
-    //private final DTOService dtoService;
+    private final DTOService dtoService;
 
     @GetMapping
     public ResponseEntity<List<ProcedureRequestDTO>> getAllProceduresWithDetails() {
-        DTOService dtoService1 = new DTOService(dtoRepository);
-        List<ProcedureRequestDTO> procedureRequestDTOs = dtoService1.getProcedureRequests();
+        List<ProcedureRequestDTO> procedureRequestDTOs = dtoService.getProcedureRequests();
         return ResponseEntity.ok(procedureRequestDTOs);
     }
 
