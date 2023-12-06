2fe3c99c93b19521d55b0cb298c43cbcbd5ea312
Clara <ik63roto@studserv.uni-leipzig.de>
2023-12-06 18:10:46 +0100

    module requests update

--- frontend/src/routes/(public)/procedure/+page.svelte
+++ frontend/src/routes/(public)/procedure/+page.svelte
@@ -1,9 +1,10 @@
 <script>
+  import { onMount } from 'svelte';
   export let data;
 
   const modules = data.modules;
 
-
+ 
 
 //Logik zum wechseln des Tabs
  let activeTab= "pills-general";
@@ -20,7 +21,7 @@
         activeTab = tabOrder[currentTabIndex];
     };
 
-//Logik zum einlesen der Daten aus Formularen
+//Logik zum einlesen der Daten aus den allgemeinen Angaben
   let generalData = {
       uni: '',
       formerStudies: '',
@@ -28,6 +29,12 @@
     };
 //Logik für Anträge
   let requests = [];
+
+  onMount(() => {
+    // Füge einen initialen Request hinzu
+    addRequest();
+  });
+
 
   function addRequest(){
     requests = [...requests, {
@@ -131,82 +138,97 @@
 {/if}
 
 {#if activeTab === 'pills-module'}
+
 <h1>{generalData.uni}</h1>
 <form action="POST" id="requests">
-    {#each requests as { moduleData }, index (index) }
-     <div class="card w-100 mb-3" key={index}>
-        <div class="card-body">
+  
+  {#each requests as { moduleData }, index (index) }
+  
+  <div class="card w-100 mb-3" key={index}>
+    <div class="card-body">
+      <div class="position-relative">
+        <button class="btn btn-primary position-absolute top-0 end-0" on:click={() => removeRequest(index)}>Antrag löschen</button>
+      </div>
+        <div class="row">
+          <div class="col-md">
+            <h2 class="h4">Fremdmodul</h2>
 
-          <div class="row">
-            <div class="col-md">
-              <h2 class="h4">Fremdmodul</h2>
-
-              <div class="row">
-                <div class="col-md-6 mb-3">
-                  <label for="" class="mb-2" >Universität</label>
-                  <input type="text" class="form-control" value={generalData.uni}>
-                </div>
-                <div class="col-md-6">
-                  <label for="" class="mb-2" >Studiengang</label>
-                  <input type="text" class="form-control" value={generalData.formerStudies}>
-                </div>
+            <div class="row">
+              <div class="col-md-6 mb-3">
+                <label for="" class="mb-2" >Universität</label>
+                <input type="text" class="form-control" value={generalData.uni}>
               </div>
+              <div class="col-md-6">
+                <label for="" class="mb-2" >Studiengang</label>
+                <input type="text" class="form-control" value={generalData.formerStudies}>
+              </div>
+            </div>
 
+            <div class="row"> 
+            <div class="col-md-10">
               <div class="mb-3">
                 <label for="" class="mb-2">Name</label>
                 <input type="text" class="form-control" placeholder="Modellierung und Programmierung" />
               </div>
-
-              <div class="row">
-                <div class="col-md-8">
-                  <div class="mb-3">
-                    <label for="" class="mb-2">Website zum Modul</label>
-                    <input type="text" class="form-control" placeholder="http://uni-leipzig.de/module_xyz" />
-                  </div>
-                </div>
-                <div class="col-md-4">
-                  <div class="mb-3">
-                    <label for="" class="mb-2">LP</label>
-                    <input type="text" class="form-control" placeholder="5" />
-                  </div>
+            </div>
+              <div class="col-md-2">
+                <div class="mb-3">
+                  <label for="" class="mb-2">LP</label>
+                  <input type="text" class="form-control" placeholder="5" />
                 </div>
               </div>
+            </div>
+
               <div class="mb-3">
                 <label for="formFile" class="form-label">Modulbeschreibung<i class="bi bi-question-circle ms-2" /> </label>
                 <input class="form-control" type="file" id="formFile" />
               </div>
-            </div>
-            <div class="col-md">
-              <h2 class="h4">Modul Vorschlag</h2>
 
-              <div class="mb-2">
-
+              <div >
+                <div class="mb-3">
+                  <label for="" class="mb-2">Website zum Modul</label>
+                  <input type="text" class="form-control" placeholder="http://uni-leipzig.de/module_xyz" />
+                </div>
               </div>
-              <div class="mb-3">
-                <label for="" class="mb-2">Name</label>
-                <select class="form-select" aria-label="Default select example" bind:value={moduleData.selectedModul}>
-                  {#each modules as modul, index}
-                    <option value={index}>{modul.moduleName}</option>
-                  {/each}
-                </select>
-              </div>
+              
 
-              <p>{modules[moduleData.selectedModul].moduleDescription ?? ''}</p>
-            </div>
+            
           </div>
-        </div>{activeTab === 'pills-general' ? 'active' : ''}
-        <button class="btn btn-primary" on:click={() => removeRequest(index)}>Remove Request</button>
-      </div> 
+          <div class="col-md">
+            <h2 class="h4">Modul Vorschlag</h2>
+
+            <div class="mb-2">
+
+            </div>
+            <div class="mb-3">
+              <label for="" class="mb-2">Name</label>
+              <select class="form-select" aria-label="Default select example" bind:value={moduleData.selectedModul}>
+                {#each modules as modul, index}
+                  <option value={index}>{modul.moduleName}</option>
+                {/each}
+              </select>
+            </div>
+
+            <p>{modules[moduleData.selectedModul].moduleDescription ?? ''}</p>
+          </div>
+        </div>
+        <div class="form-floating">
+          <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px"></textarea>
+          <label for="floatingTextarea">Kommentare</label>
+        </div>
+      </div>{activeTab === 'pills-general' ? 'active' : ''}
       
-  {/each}
-      
-  
+    </div> 
+    
+{/each}
+    
+
 </form>
 
 <div class="w-100 d-inline-flex justify-content-center">
-  <button class="btn btn-primary" on:click={addRequest}>
-    <i class="bi bi-plus-circle" style="font-size: 2rem;" />
-  </button>
+<button class="btn btn-primary" on:click={addRequest}>
+  <i class="bi bi-plus-circle" style="font-size: 2rem;" />
+</button>
 </div>
 {/if}
 <hr />
