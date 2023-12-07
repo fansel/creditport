40f6a3675ecfe9b060c122c8c8f3259778182791
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-12-07 11:56:42 +0100

    add getProcedureWithRequests

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/procedures/ProcedureService.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/procedures/ProcedureService.java
@@ -1,8 +1,15 @@
 package de.swtp13.creditportbackend.v1.procedures;
 
 import de.swtp13.creditportbackend.v1.procedures.util.IDGenerator;
+import de.swtp13.creditportbackend.v1.requests.RequestRepository;
+import de.swtp13.creditportbackend.v1.requests.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Die Klasse enthält alle Methoden zu Vorgängen.
@@ -12,6 +19,8 @@ public class ProcedureService {
 
     @Autowired
     private ProcedureRepository procedureRepository;
+    @Autowired
+    private RequestRepository requestRepository;
 
     /**
      * Die Methode generiert einen neuen Vorgang und setzt die ID.
@@ -30,22 +39,23 @@ public class ProcedureService {
         return procedure;
     }
 
-    /*
-    public Procedure updateProcedure(String procedureId, Procedure procedureDetails) {
-        // Retrieve the existing procedure from the repository
-        Procedure existingProcedure = procedureRepository.findById(procedureId)
-                .orElseThrow(() -> new EntityNotFoundException("Procedure not found with id: " + procedureId));
 
-        // Update the fields of the existing procedure with the values from the details provided
-        existingProcedure.setSomeField(procedureDetails.getSomeField());            //TODO: anpassen
-        existingProcedure.setAnotherField(procedureDetails.getAnotherField());      //TODO: anpassen
-        // ... update other fields as needed
-
-        // Save the updated procedure back to the repository
-        return procedureRepository.save(existingProcedure);
-    }
-     */
-    public boolean existsById(String id) {
-        return procedureRepository.existsById(id);
-    }
+    public List<Procedure> getProceduresWithRequests() {
+        List<Request> requests = requestRepository.findAllWithProcedure();
+        Map<String, Procedure> procedureMap = new HashMap<>();
+        for (Request request : requests) {
+            Procedure procedure = request.getProcedure();
+            Procedure finalProcedure = procedureMap.computeIfAbsent(procedure.getProcedureId(), k -> {
+                Procedure newProcedure = new Procedure();
+                newProcedure.setProcedureId(procedure.getProcedureId());
+                newProcedure.setStatus(procedure.getStatus());
+                newProcedure.setAnnotation(procedure.getAnnotation());
+                newProcedure.setCreatedAt(procedure.getCreatedAt());
+                newProcedure.setRequests(new ArrayList<>());
+                return newProcedure;
+                });
+            finalProcedure.getRequests().add(request);
+        }
+        return new ArrayList<>(procedureMap.values());
+        }
 }
