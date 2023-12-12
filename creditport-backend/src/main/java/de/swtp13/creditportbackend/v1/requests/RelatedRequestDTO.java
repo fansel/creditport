da1cd8483bfe5a637a76591209b6aad5395b8653
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-12-12 18:27:14 +0100

    add RelatedRequestDTO

---
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/requests/RelatedRequestDTO.java
@@ -0,0 +1,25 @@
+package de.swtp13.creditportbackend.v1.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RelatedRequestDTO {
+    private int procedureId;
+    private int requestId;
+    private String externalModule;
+    private String internalModule;
+    private String annotation;
+    private int creditPoints;
+    private Status status;
+    private LocalDateTime createdAt;
+    private byte[] pdfContent;
+
+    private List<Integer> relatedRequests;
+}
