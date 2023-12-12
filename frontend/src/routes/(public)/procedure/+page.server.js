70059845bef5d809ed2dd80869afc3768807fb3b
Clara <ik63roto@studserv.uni-leipzig.de>
2023-12-12 22:48:53 +0100

    update prodedure page

--- frontend/src/routes/(public)/procedure/+page.server.js
+++ frontend/src/routes/(public)/procedure/+page.server.js
@@ -1,4 +1,8 @@
 import * as api from '$lib/api.js';
+import { zfd } from 'zod-form-data';
+import z from 'zod';
+
+
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {
@@ -8,3 +12,74 @@ export async function load({ params }) {
     modules: modules
   };
 }
+
+/** @type {import('./$types').Actions} */
+//Einträge
+function createMultipleEntries(numEntries) {
+  const entries = [];
+  for (let i = 0; i < numEntries; i++) {
+    const entry = {
+      ['fremduni' + i]: zfd.text(),
+      ['fremdstudiengang' + i]: zfd.text(),
+      ['fremdstudiengangName' + i]: zfd.text(),
+      ['lp' + i]: zfd.text(), // vielleicht besser als Zahl
+      ['modulbeschreibung' + i]: zfd.file(z.instanceof(File).optional()),
+      ['website' + i]: zfd.text(),
+      ['studiengangUniLeipzig' + i]: zfd.text(),
+      ['kommentar' + i]: zfd.text(),
+    };
+    entries.push(entry);
+  }
+  return entries;
+}
+
+
+export const actions = {
+  requests: async ({ request }) => {
+    const formData = await request.formData();
+
+    const modulesCount = formData.get('modulesCount') || 0;
+
+    console.log(modulesCount)
+
+    // console.log(createMultipleEntries(2))
+
+    const schema = zfd.formData({
+      entries: createMultipleEntries(1)
+    })
+    
+    // const result = schema.safeParse(formData);
+
+    // if (!result.success){
+    //     const data = {
+    //         data: Object.fromEntries(formData),
+    //         errors: result.error.flatten().fieldErrors
+    //       };
+    //   return fail(400, data)  
+    // }
+
+    // console.log(result.data)
+
+    //result.data.text
+    //result.data.pdf -> 
+
+
+    // const fileUpload1 = formData();
+    // test.append('file', result.data.file);
+    // test.append('id', id)
+
+    // //http://localhost:8080/api/v1/ + path
+    // const res = await api.post('', test, null, { req_type: api.content_type.json , res_type: api.content_type.json})
+
+
+
+    // console.log(res)
+
+    // return { success: true }
+
+
+
+
+  }
+}
+
--- frontend/src/routes/(public)/procedure/+page.svelte
+++ frontend/src/routes/(public)/procedure/+page.svelte
@@ -1,54 +1,56 @@
 <script>
   import { onMount } from 'svelte';
+  import { enhance } from '$app/forms';
+  import { page } from '$app/stores';
   export let data;
 
   const modules = data.modules;
 
- 
+  //Logik zum wechseln des Tabs
+  let activeTab = 'pills-general';
 
-//Logik zum wechseln des Tabs
- let activeTab= "pills-general";
+  const switchTab = (tab) => {
+    activeTab = tab;
+  };
 
-    const switchTab = (tab) => {
-        activeTab = tab;
-    };
+  const tabOrder = ['pills-general', 'pills-module', 'pills-send'];
+  let currentTabIndex = 0;
 
-    const tabOrder = ['pills-general', 'pills-module', 'pills-send'];
-    let currentTabIndex = 0;
+  const goToNextTab = () => {
+    currentTabIndex = (currentTabIndex + 1) % tabOrder.length;
+    activeTab = tabOrder[currentTabIndex];
+  };
 
-    const goToNextTab = () => {
-        currentTabIndex = (currentTabIndex + 1) % tabOrder.length;
-        activeTab = tabOrder[currentTabIndex];
-    };
-
-//Logik zum einlesen der Daten aus den allgemeinen Angaben
+  //Logik zum einlesen der Daten aus den allgemeinen Angaben
   let generalData = {
-      uni: '',
-      formerStudies: '',
-      futureStudies:'',
-    };
-//Logik für Anträge
+    uni: '',
+    formerStudies: '',
+    futureStudies: ''
+  };
+  //Logik für Anträge
   let requests = [];
+  let modulesForm;
 
   onMount(() => {
     // Füge einen initialen Request hinzu
     addRequest();
   });
 
-
-  function addRequest(){
-    requests = [...requests, {
+  function addRequest() {
+    requests = [
+      ...requests,
+      {
         moduleData: {
-          selectedModul: 0,
-        },
-    }];
+          selectedModul: 0
+        }
+      }
+    ];
   }
 
   function removeRequest(index) {
     requests = requests.filter((_, i) => i !== index);
   }
 </script>
-
 
 <ul class="status-list my-3 m-0 p-0" id="pills-tab" role="tablist">
   <li class="status-item" role="presentation">
@@ -61,8 +63,8 @@
       type="button"
       role="tab"
       aria-controls="pills-general"
-      aria-selected="{activeTab === 'pills-general'}"
-      >
+      aria-selected={activeTab === 'pills-general'}
+    >
       <div class="rounded-circle border-3 border border-primary status-circle {activeTab === 'pills-general' ? 'active' : ''}">
         <span>1</span>
       </div>
@@ -81,8 +83,8 @@
       type="button"
       role="tab"
       aria-controls="pills-module"
-      aria-selected="{activeTab === 'pills-module'}"
-      >
+      aria-selected={activeTab === 'pills-module'}
+    >
       <div class="rounded-circle border-3 border border-primary status-circle {activeTab === 'pills-module' ? 'active' : ''}">
         <span>2</span>
       </div>
@@ -92,15 +94,17 @@
   </li>
 
   <li class="status-item" role="presentation">
-    <button class="status-button fw-bold text-primary" 
-            on:click={() => switchTab('pills-send')}
-            id="pills-send-tab" 
-            data-bs-toggle="pill" 
-            data-bs-target="#pills-send" 
-            type="button" 
-            role="tab" 
-            aria-controls="pills-send" 
-            aria-selected="false">
+    <button
+      class="status-button fw-bold text-primary"
+      on:click={() => switchTab('pills-send')}
+      id="pills-send-tab"
+      data-bs-toggle="pill"
+      data-bs-target="#pills-send"
+      type="button"
+      role="tab"
+      aria-controls="pills-send"
+      aria-selected="false"
+    >
       <div class="rounded-circle border-3 border border-primary status-circle {activeTab === 'pills-send' ? 'active' : ''}">
         <span>3</span>
       </div>
@@ -110,8 +114,9 @@
   </li>
 </ul>
 
-{#if activeTab === 'pills-general'}
-<form action="/?add" method="POST">
+<!-- Tab General -->
+<div class={activeTab == 'pills-general' ? '' : 'visually-hidden'}>
+  <!-- <form action="/?add" method="POST"> -->
   <div class="row">
     <div class="col-md">
       <div class="mb-3">
@@ -120,119 +125,121 @@
       </div>
       <div class="mb-3">
         <label for="" class="mb-2">Studiengang der anzurechnenden Module</label>
-        <input type="text" id="formerStudies" bind:value={generalData.formerStudies} class="form-control" placeholder="B.Sc. Informatik"/>
+        <input type="text" id="formerStudies" bind:value={generalData.formerStudies} class="form-control" placeholder="B.Sc. Informatik" />
       </div>
       <div class="mb-3">
         <label for="" class="mb-2">Studiengang der Universität Leipzig an dem die Anrechnung erfolgen soll</label>
         <input type="text" id="futureStudies" bind:value={generalData.futureStudies} class="form-control" placeholder="B.Sc. Wirtschaftsinformatik" />
       </div>
       <div class="form-floating">
-        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px"></textarea>
+        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px" />
         <label for="floatingTextarea">Kommentare</label>
       </div>
     </div>
-   
   </div>
- 
-</form>
-{/if}
+  <hr />
+  <button type="button" class="btn btn-primary" on:click={goToNextTab}>Weiter</button>
+  <!-- </form> -->
+</div>
 
-{#if activeTab === 'pills-module'}
+<!-- Tab Module -->
+<div class={activeTab == 'pills-module' ? '' : 'visually-hidden'}>
+  <h1>Überschrift</h1>
 
-<h1>{generalData.uni}</h1>
-<form action="POST" id="requests">
-  
-  {#each requests as { moduleData }, index (index) }
-  
-  <div class="card w-100 mb-3" key={index}>
-    <div class="card-body">
-      <div class="position-relative">
-        <button class="btn btn-primary position-absolute top-0 end-0" on:click={() => removeRequest(index)}>Antrag löschen</button>
-      </div>
-        <div class="row">
-          <div class="col-md">
-            <h2 class="h4">Fremdmodul</h2>
+  <form action="?/requests" method="POST" enctype="multipart/form-data" use:enhance id="requests" bind:this={modulesForm}>
+    <input type="hidden" name="university" value={1} />
+    <input type="hidden" name="course" value={1} />
+    <input type="hidden" name="modulesCount" bind:value={requests.length} />
+    {#each requests as { moduleData }, index (index)}
+      <div class="card w-100 mb-3" key={index}>
+        <div class="card-body">
+          <div class="position-relative">
+            <button class="btn btn-primary position-absolute top-0 end-0" on:click={() => removeRequest(index)} type="button">Antrag löschen</button>
+          </div>
+          <div class="row">
+            <div class="col-md">
+              <h2 class="h4">Fremdmodul</h2>
 
-            <div class="row">
-              <div class="col-md-6 mb-3">
-                <label for="" class="mb-2" >Universität</label>
-                <input type="text" class="form-control" value={generalData.uni}>
-              </div>
-              <div class="col-md-6">
-                <label for="" class="mb-2" >Studiengang</label>
-                <input type="text" class="form-control" value={generalData.formerStudies}>
-              </div>
-            </div>
-
-            <div class="row"> 
-            <div class="col-md-10">
-              <div class="mb-3">
-                <label for="" class="mb-2">Name</label>
-                <input type="text" class="form-control" placeholder="Modellierung und Programmierung" />
-              </div>
-            </div>
-              <div class="col-md-2">
-                <div class="mb-3">
-                  <label for="" class="mb-2">LP</label>
-                  <input type="text" class="form-control" placeholder="5" />
+              <div class="row">
+                <div class="col-md-6 mb-3">
+                  <label for="" class="mb-2">Universität</label>
+                  <input type="text" class="form-control" name={`fremduni${index}`} value={generalData.uni} />
+                </div>
+                <div class="col-md-6">
+                  <label for="" class="mb-2">Studiengang</label>
+                  <input type="text" class="form-control" name={`fremdstudiengang${index}`} value={generalData.formerStudies} />
                 </div>
               </div>
-            </div>
+
+              <div class="row">
+                <div class="col-md-10">
+                  <div class="mb-3">
+                    <label for="" class="mb-2">Name des Studiengangs</label>
+                    <input type="text" class="form-control" name="fremdstudiengangName" placeholder="Modellierung und Programmierung" />
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="mb-3">
+                    <label for="" class="mb-2">LP</label>
+                    <input type="text" class="form-control" name="lp" placeholder="5" />
+                  </div>
+                </div>
+              </div>
 
               <div class="mb-3">
                 <label for="formFile" class="form-label">Modulbeschreibung<i class="bi bi-question-circle ms-2" /> </label>
-                <input class="form-control" type="file" id="formFile" />
+                <input class="form-control" type="file" name="modulbeschreibung" id="formFile" />
               </div>
 
-              <div >
+              <div>
                 <div class="mb-3">
                   <label for="" class="mb-2">Website zum Modul</label>
-                  <input type="text" class="form-control" placeholder="http://uni-leipzig.de/module_xyz" />
+                  <input type="text" class="form-control" name="website" placeholder="http://uni-leipzig.de/module_xyz" />
                 </div>
               </div>
-              
+            </div>
+            <div class="col-md">
+              <h2 class="h4">Modul Vorschlag</h2>
 
-            
+              <div class="mb-2" />
+              <div class="mb-3">
+                <label for="" class="mb-2">Name</label>
+                <select class="form-select" name="studiengangUniLeipzig" aria-label="Default select example" bind:value={moduleData.selectedModul}>
+                  {#each modules as modul, index}
+                    <option value={index}>{modul.moduleName}</option>
+                  {/each}
+                </select>
+              </div>
+
+              <p>{modules[moduleData.selectedModul].moduleDescription ?? ''}</p>
+            </div>
           </div>
-          <div class="col-md">
-            <h2 class="h4">Modul Vorschlag</h2>
-
-            <div class="mb-2">
-
-            </div>
-            <div class="mb-3">
-              <label for="" class="mb-2">Name</label>
-              <select class="form-select" aria-label="Default select example" bind:value={moduleData.selectedModul}>
-                {#each modules as modul, index}
-                  <option value={index}>{modul.moduleName}</option>
-                {/each}
-              </select>
-            </div>
-
-            <p>{modules[moduleData.selectedModul].moduleDescription ?? ''}</p>
+          <div class="form-floating">
+            <textarea class="form-control" name="kommentar" placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px" />
+            <label for="floatingTextarea">Kommentare</label>
           </div>
         </div>
-        <div class="form-floating">
-          <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px"></textarea>
-          <label for="floatingTextarea">Kommentare</label>
-        </div>
-      </div>{activeTab === 'pills-general' ? 'active' : ''}
-      
-    </div> 
-    
-{/each}
-    
+        {activeTab === 'pills-general' ? 'active' : ''}
+      </div>
+    {/each}
+    <hr />
+    <button type="button" class="btn btn-primary" on:click={goToNextTab}>Weiter</button>
 
-</form>
-
-<div class="w-100 d-inline-flex justify-content-center">
-<button class="btn btn-primary" on:click={addRequest}>
-  <i class="bi bi-plus-circle" style="font-size: 2rem;" />
-</button>
+    <div class="w-100 d-inline-flex justify-content-center">
+      <button class="btn btn-primary" on:click={addRequest}>
+        <i class="bi bi-plus-circle" style="font-size: 2rem;" />
+      </button>
+    </div>
+  </form>
 </div>
-{/if}
-<hr />
-<button type="button" class="btn btn-primary" on:click={goToNextTab} >Weiter</button>
+
+<!-- Tab Send -->
+<div class={activeTab == 'pills-send' ? '' : 'visually-hidden'}>
+  <button class="btn btn-primary" on:click={() => modulesForm.submit()}>Senden</button>
+</div>
+
+<!-- <hr />
+<button type="button" class="btn btn-primary" on:click={goToNextTab} >Weiter</button> -->
 
 <style>
   .status-circle {
