d788734bad0ef4abd2279164e1aa8f480def8e39
Jensberger90 <silkeackermann35@web.de>
2023-12-13 16:54:27 +0100

    fixed wrong date

--- frontend/src/routes/(public)/+layout.server.js
+++ frontend/src/routes/(public)/+layout.server.js
@@ -3,12 +3,13 @@ import * as api from '$lib/api.js';
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
   //Hier wird ein Request ans Backend gesendet um zu prüfen ob es ONLINE ist
-    const backend_data = (await api.get('actuator/health')) || [];
-    let status = false;
+  const backend_data = await api.get('actuator/health');
 
-    if (backend_data.status === 'UP') {
-      status = true;
-    }
+  if (backend_data && backend_data.status === 'UP') {
+    return {
+      status: true
+    };
+  }
 
   return {
     status: false
