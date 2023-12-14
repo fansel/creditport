f0c08ab47a9376d16311aa073ad54e3df2cc1215
Maike Zaspel <maike.zaspel.mz@gmail.com>
2023-12-14 14:55:46 +0100

    add DELETE, PUT, POST (single module or list import)

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/modules/ModuleController.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/modules/ModuleController.java
@@ -1,9 +1,8 @@
 package de.swtp13.creditportbackend.v1.modules;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,44 @@ public class ModuleController {
     public ResponseEntity<List<Module>> getAllModules() {
         System.out.println("Get all modules");
         return ResponseEntity.ok(moduleRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createModule(
+            @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false, defaultValue = "") String token,
+            @RequestBody Module Module) {
+        System.out.println("Create Module: " + Module.getModuleName());
+        return ResponseEntity.ok(moduleRepository.save(Module));
+    }
+
+
+    // PUT: Update a Module
+    @PutMapping("/{number}")
+    public ResponseEntity<Module> updateModule(@PathVariable String number, @RequestBody Module ModuleDetails) {
+        return moduleRepository.findById(number)
+                .map(Module -> {
+                    Module.setModuleName(ModuleDetails.getModuleName());
+                    Module.setModuleDescription(ModuleDetails.getModuleDescription());
+                    Module.setUniversity(ModuleDetails.getUniversity());
+                    Module updatedModule = moduleRepository.save(Module);
+                    return ResponseEntity.ok(updatedModule);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    // DELETE: Delete a Module
+    @DeleteMapping("/{number}")
+    public ResponseEntity<?> deleteModule(@PathVariable String number) {
+        return moduleRepository.findById(number)
+                .map(Module -> {
+                    moduleRepository.delete(Module);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    // import Module data from json fil
+    @PostMapping("/import")
+    public ResponseEntity<List<Module>> importUniversities(@RequestBody List<Module> modules) {
+        moduleRepository.saveAll(modules);
+        return ResponseEntity.ok(modules);
     }
 }
