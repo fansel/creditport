b87b8e78a96a151e14c29d235a92997bf2f1f9e1
Daniel Bode <daniel@bode-leipzig.de>
2024-03-12 00:46:00 +0100

    adjust login endpoint to use status code 400

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/auth/AuthenticationController.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/auth/AuthenticationController.java
@@ -1,8 +1,11 @@
 package de.swtp13.creditportbackend.v1.auth;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,12 +21,19 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @Operation(summary = "login endpoint for users", responses = {
-            @ApiResponse(responseCode = "200")
+            @ApiResponse(responseCode = "200",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthenticationResponse.class))),
+            @ApiResponse(responseCode = "400", content = @Content)
     })
     @PostMapping("/login")
-    public ResponseEntity<AuthenticationResponse> authenticate(
+    public ResponseEntity<?> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+        AuthenticationResponse authresponse = authenticationService.authenticate(request);
+        if (authresponse != null)
+            return ResponseEntity.ok(authresponse);
+        else
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
