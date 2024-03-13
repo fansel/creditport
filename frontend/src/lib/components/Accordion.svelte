244be8247fc7fec19986bf9c17587039280ea3aa
Elias Taibi Haschke <elias.taibi.haschke@gmail.com>
2024-03-13 13:22:49 +0100

    final design

---
+++ frontend/src/lib/components/Accordion.svelte
@@ -0,0 +1,71 @@
+<script>
+  export let open = false;
+  import { slide } from 'svelte/transition';
+  const handleClick = () => (open = !open);
+</script>
+
+<div class="accordion">
+  <div class="card">
+    <button class="reset-btn card-header btn m-0" on:click={handleClick}>
+      <h5 class="mb-0 d-flex justify-content-between">
+        <slot name="head" />
+        <!-- <button class="btn btn-link" on:click={handleClick}> </button> -->
+        <span class="bi-chevron-down small"></span>
+      </h5>
+    </button>
+
+    {#if open}
+      <div class="collapse show" transition:slide>
+        <div class="card-body">
+          <slot name="details" />
+        </div>
+      </div>
+    {/if}
+  </div>
+</div>
+
+<style>
+  .accordion {
+    margin: 1rem 0;
+  }
+
+  .card {
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    border-radius: 0.25rem;
+  }
+
+  .card-header {
+    background-color: #f8f9fa;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.125);
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 0;
+  }
+
+  .card-header h5 {
+    margin-bottom: 0;
+  }
+
+  .btn-link {
+    color: #007bff;
+    background-color: transparent;
+    text-decoration: none;
+    font-weight: 400;
+  }
+
+  .btn-link:hover {
+    color: #0056b3;
+    text-decoration: underline;
+  }
+
+  .collapse {
+    display: none;
+  }
+
+  .collapse.show {
+    display: block;
+  }
+
+  .card-body {
+    padding: 1.25rem;
+  }
+</style>
--- frontend/src/routes/(intern)/procedures/[id]/+page.svelte
+++ frontend/src/routes/(intern)/procedures/[id]/+page.svelte
@@ -7,6 +7,8 @@
 
   import Header from '$lib/components/InternHeader.svelte';
   import Comment from './Comment.svelte';
+  import RequestStatus from '$root/lib/components/RequestStatus.svelte';
+  import Accordion from '$root/lib/components/Accordion.svelte';
 
   export let data;
   let procedure = data.procedure;
@@ -21,11 +23,27 @@
     selectedModule[i] = index;
   }
 
-  // DELETE
-  $: {
-    console.log('selectedModule: ', selectedModule);
+  async function submitForm(event) {
+    const body = request;
+    delete body.relatedRequests;
+    const res = await fetch('/update-request', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(body)
+    });
+    // successToast('Erfolreich gespeichert');
+
+    console.log(res);
+    requestBackup = JSON.parse(JSON.stringify(request));
   }
-  let verified = [true, false];
+
+  async function cancelChanges(event) {
+    request = requestBackup;
+    submitForm();
+  }
 
   function sumLP(list) {
     let sum = 0;
@@ -48,7 +66,11 @@
 <!-- <Header wide={true} /> -->
 <div class="m-2">
   <div class="border-bottom m-3 d-flex">
-    <div class="dates">
+    <div class="vorgangsnummer">
+      <strong>Vorgangsnummer: </strong>
+      {request.procedureId}
+    </div>
+    <div class="dates d-none d-lg-block">
       <i class="bi bi-calendar"></i>
       {format(new Date(request.createdAt), 'dd.MM.yyyy')}
 
@@ -56,34 +78,114 @@
       {format(new Date(request.createdAt), 'dd.MM.yyyy')}
     </div>
 
-    <div class="mx-2 d-flex">
-      <div class="vorgangsnummer">
-        <strong>Vorgangsnummer: </strong>
-        {request.procedureId}
-      </div>
-      <div class="studium mx-3">
-        <strong>{procedure.course.courseName} </strong>
-      </div>
+    <div class="studium mx-3">
+      <strong>{procedure.course.courseName} </strong>
     </div>
 
-    <div class="status justfiy-content-between mx-auto">NEU</div>
+    <div class="mx-2 d-flex align-items-center justify-content-between flex-grow-1">
+      <div></div>
+
+      <div class="mx-2 status">
+        <RequestStatus status={request.statusRequest} />
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      {#each request.internalModules as module, index}
+        <Accordion>
+          <div slot="head">
+            <i class="bi {module.verified ? 'bi-patch-check-fill' : 'bi-patch-check'}" />
+          </div>
+          <div slot="details">
+            <div class=" mb-1 p-1">
+              <div class="row">
+                <div class="col-md-9">
+                  <div class="mb-2">
+                    <div class="row">
+                      <label class="mb-2">Name des Moduls </label>
+                    </div>
+
+                    <input type="text" class="form-control" placeholder="Modellierung und Programmierung" bind:value={module.moduleName} />
+                  </div>
+                </div>
+                <div class="col-md-3">
+                  <div class="mb-3">
+                    <label class="mb-2">LP</label>
+                    <input type="number" class="form-control" placeholder={module.creditPoints} />
+                  </div>
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label class="mb-2">Website zum Modul</label>
+                <input type="text" class="form-control" placeholder="Füge Link ein" />
+              </div>
+            </div>
+          </div>
+        </Accordion>
+      {/each}
+    </div>
+    <div class="col-md-6">
+      {#each request.internalModules as module, index}
+        <Accordion>
+          <div slot="head" class="d-flex justify-content-">
+            <span>{truncateText(module.moduleName, 55)}</span>
+          </div>
+          <div slot="details">
+            <div class="p-1">
+              <div class="row">
+                <div class="col-md-9">
+                  <div class="row">
+                    <label class="mb-1"><strong>Name des Moduls</strong> </label>
+                  </div>
+
+                  <select class="form-select" aria-label="Default select example" bind:value={selectedModule[index]}>
+                    {#each modules as modul, index}
+                      <option value={index}>{modul.moduleName}</option>
+                    {/each}
+                  </select>
+                </div>
+                <div class="col-md-3">
+                  <div class="mb-3">
+                    <label class="mb-1"><strong>LP</strong></label>
+                    <input type="text" class="form-control" disabled placeholder={(request.internalModules[index] = modules[selectedModule[index]]).creditPoints} />
+                    <!-- <p class="border p-2 rounded">{module.creditPoints}</p> -->
+                  </div>
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label class="mb-2"><strong>Modulbeschreibung</strong></label>
+                <p class="moduleDescription border p-2 rounded">
+                  <!-- {#if modules[moduleData.selectedModul[selectedModulIndex]].moduleDescription} -->
+                  {(module = modules[selectedModule[index]].moduleDescription)}
+                  <!-- {/if} -->
+                </p>
+                <!-- <input type="text" class="form-control" disabled placeholder={truncateText(modules[0].moduleDescription, 25)} /> -->
+              </div>
+            </div>
+          </div>
+        </Accordion>
+      {/each}
+    </div>
   </div>
 
   <!-- OVERVIEW MODULE MAPPING -->
   <div class="modules-mapping row">
-    <div class="external m-2 col-md-5">
-      {#each request.externalModules as module, index}
-        <div class="accordion" id="accordionExample">
+    <div class="external col-md-6">
+      <div class="accordion" id="accordionExample">
+        {#each request.externalModules as module, index}
           <div class="accordion-item">
             <h2 class="accordion-header">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{index}{index}" aria-expanded="true" aria-controls="collapseOne">
-                <span>{truncateText(module.moduleName, 35)}</span>
-                <span><i class="bi {verified[index] ? 'bi-patch-check-fill' : 'bi-ban'}"></i></span>
+              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-external-{index}" aria-expanded="true" aria-controls="collapseOne">
+                <span>{truncateText(module.moduleName, 55)}</span>
+                <span><i class="bi {module.verified ? 'bi-patch-check-fill' : 'bi-patch-check'}"></i></span>
               </button>
             </h2>
-            <div id="collapse{index}{index}" class="accordion-collapse collapse {index === 0 ? 'show' : ''}" data-bs-parent="#accordionExample">
+            <div id="collapse-external-{index}" class="accordion-collapse collapse {index === 0 ? 'show' : ''}" data-bs-parent="#accordionExample">
               <div class="accordion-body">
-                <div class=" mb-1 rounded p-2">
+                <div class=" mb-1 p-1">
                   <div class="row">
                     <div class="col-md-9">
                       <div class="mb-2">
@@ -110,17 +212,13 @@
               </div>
             </div>
           </div>
-        </div>
-      {/each}
-      <div class="mx-2">
-        <strong>LP Summe: </strong>
-        {sumLP(request.externalModules)}
+        {/each}
       </div>
     </div>
 
-    <div class="internal m-2 col-md-5">
-      {#each request.internalModules as module, index}
-        <div class="accordion" id="accordionExample">
+    <div class="internal col-md-6">
+      <div class="accordion" id="accordionExample">
+        {#each request.internalModules as module, index}
           <div class="accordion-item">
             <h2 class="accordion-header">
               <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse{index}" aria-expanded="true" aria-controls="collapseOne">
@@ -129,7 +227,7 @@
             </h2>
             <div id="collapse{index}" class="accordion-collapse collapse {index === 0 ? 'show' : ''}" data-bs-parent="#accordionExample">
               <div class="accordion-body">
-                <div class=" mb-1 rounded p-2">
+                <div class=" mb-1 p-1">
                   <div class="row">
                     <div class="col-md-9">
                       <div class="mb-2">
@@ -155,7 +253,7 @@
 
                   <div class="mb-3">
                     <label class="mb-2">Modulbeschreibung</label>
-                    <p class="moduleDescription border p-3 rounded">
+                    <p class="moduleDescription border p-2 rounded">
                       <!-- {#if modules[moduleData.selectedModul[selectedModulIndex]].moduleDescription} -->
                       {(module = modules[selectedModule[index]].moduleDescription)}
                       <!-- {/if} -->
@@ -166,26 +264,33 @@
               </div>
             </div>
           </div>
-        </div>
-      {/each}
-      <div class="mx-2">
+        {/each}
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="m-2">
+        <strong>LP Summe: </strong>
+        {sumLP(request.externalModules)}
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="m-2">
         <strong>LP Summe: </strong>
         {sumLP(request.internalModules)}
       </div>
     </div>
-
-    <div class="mb-3 col-md-1 d-flex align-self-baseline">
-      <label class="mb-2" style="margin-right: 10px;">LP</label>
-      <input type="number" class="form-control" placeholder="5" />
-    </div>
   </div>
-  <div class="comment my-3" style="margin-right: 20px">
+  <!-- <div class="comment my-3" style="margin-right: 20px">
     <Comment bind:annotationCommittee={request.annotationCommittee} bind:annotationStudent={request.annotationStudent} />
   </div>
   <div class="buttons d-flex ms-auto">
-    <button type="submit" class="btn btn-sm btn-primary" style="margin-right: 5px;" on:click={submitForm}>Speichern</button>
-    <div class="btn btn-sm btn-outline-secondary" on:click={cancelChanges}>Abbrechen</div>
-  </div>
+    <button type="submit" class="btn btn-sm btn-primary d-flex" on:click={submitForm}>Speichern</button>
+    <div class="btn btn-sm btn-outline-secondary mx-1 d-flex" on:click={cancelChanges}>Abbrechen</div>
+    <button type="submit" class="btn btn-sm btn-success mx-1 d-flex justfiy-content-end">Weiterleiten</button>
+    <button type="submit" class="btn btn-sm btn-danger mx-1 d-flex justfiy-content-end">Formal ablehnen</button>
+  </div> -->
   PDF
 </div>
 
@@ -204,7 +309,10 @@
     box-shadow: none;
   }
   .moduleDescription {
-    height: 120px;
+    min-height: 38px;
+    max-height: 120px;
     overflow-y: auto;
+  }
+  .dates {
   }
 </style>
