21b7eaadb45a82dcf446a45a3b267b0d7350073e
Daniel Bode <daniel@bode-leipzig.de>
2024-03-14 16:58:18 +0100

    secure endpoints behind filter chain

--- creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/config/SecurityConfig.java
+++ creditport-backend/src/main/java/de/swtp13/creditportbackend/v1/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.swtp13.creditportbackend.v1.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,18 +27,29 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         (authorizeHttpRequests) -> authorizeHttpRequests
-                                .requestMatchers(API_VERSION+"/test-endpoint/secure")
-                                .authenticated()
                                 .requestMatchers(API_VERSION+"/users/update/password")
                                 .authenticated()
                                 .requestMatchers(API_VERSION+"/users/**")
                                 .hasAuthority("ADMIN")
                                 .requestMatchers(API_VERSION+"/test-endpoint/open")
                                 .permitAll()
-                                .requestMatchers(API_VERSION + "/dto")
-                                .authenticated()
+                                .requestMatchers(HttpMethod.POST,
+                                        API_VERSION + "/universities",
+                                        API_VERSION + "/procedures",
+                                        API_VERSION + "/modules/external",
+                                        API_VERSION + "/auth/login")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.GET,
+                                        API_VERSION + "/universities",
+                                        API_VERSION + "/modules/external",
+                                        API_VERSION + "/modules/internal",
+                                        API_VERSION + "/courses",
+                                        API_VERSION + "/procedures/??????")
+                                .permitAll()
+                                .requestMatchers(API_VERSION + "/actuator/health")
+                                .permitAll()
                                 .anyRequest()
-                                .permitAll())
+                                .authenticated())
                 .sessionManagement(
                         (sessionManagement) -> sessionManagement
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
